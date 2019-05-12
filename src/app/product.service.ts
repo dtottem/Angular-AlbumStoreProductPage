9694dabe49824457c2632131b7748cc31927f3c6
Divya <dtottem16@gmail.com>
2019-05-12 00:11:55 -0400

    Interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from 'selenium-webdriver/http';
 import { Http , Response } from '@angular/http';
 import 'rxjs/add/operator/map'
+import {Album} from  './album';
+
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
@@ -9,8 +12,9 @@ private _albumUrl ='../assets/album.json';
 
   constructor(private _http : Http) { }
 
-  getAlbum(id:number){
-  return this._http.get(this._albumUrl).map((response) => response.json());
+  getAlbum(id:number): Observable<Album>
+  {
+  return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
 }
