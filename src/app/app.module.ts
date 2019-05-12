e8ec93421082e6a57e6d587c90d41c068ad4bf12
Divya <dtottem16@gmail.com>
2019-05-11 20:53:15 -0400

    Success Test cases

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { Http ,Response, HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
-// import { ProductDescriptionComponentComponent } from './product-description-component/product-description.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { ProductService } from './product.service';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,    
-    ProductDescriptionComponent
+    ProductDescriptionComponent   
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
