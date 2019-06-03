0f778a199cc9500164169bdd9dd8144ba70e36dc
Divya Tottempudi <Divya.Tottempudi@steeldynamics.com>
2019-06-03 14:12:05 -0400

    test6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,12 +3,14 @@ import { HttpClient } from 'selenium-webdriver/http';
 import { Http , Response } from '@angular/http';
 import 'rxjs/add/operator/map'
 import {Album} from  './album';
+import {Product} from './product';
 
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
 private _albumUrl ='../assets/album.json';
+private _productsUrl = '../assets/products.json';
 
   constructor(private _http : Http) { }
 
@@ -17,4 +19,9 @@ private _albumUrl ='../assets/album.json';
   return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
+  getProducts(): Observable<Product[]>
+  {
+   return  this._http.get(this._productsUrl).map((response)  => <Product[]>response.json());
+  }
+  
 }
